a792c28454a423f7895db5f5a932c843eec5afd5
Laptev Mikhail <bugi-wugi93@mail.ru>
2016-03-10 10:21:55 +0600

    6
    гаусс и начало пирамид

--- main.cpp
+++ main.cpp
@@ -12,26 +12,51 @@
 #include <maskfactory.h>
 using namespace std;
 
-shared_ptr<Image> Sobel(const Image& im, EdgeMode mode)
+shared_ptr<Image> Sobel(const Image& _im, EdgeMode _mode)
 {
     MaskFactory factory;
-    shared_ptr<Image> gradX = im.convolution(factory.SobelX(),mode);
-    shared_ptr<Image> gradY = im.convolution(factory.SobelY(),mode);
+    shared_ptr<Image> gradX = _im.convolution(factory.SobelX(),_mode);
+    shared_ptr<Image> gradY = _im.convolution(factory.SobelY(),_mode);
 
-    int width = im.getWidth();
-    int height = im.getHeight();
+    int width = _im.getWidth();
+    int height = _im.getHeight();
     shared_ptr<Image> result = make_shared<Image>(height, width);
 
     for(int i=0; i<height; i++)
         for(int j=0;j<width; j++)
         {
-            float xPix = gradX->getPixel(i,j,mode);
-            float yPix = gradX->getPixel(i,j,mode);
+            float xPix = gradX->getPixel(i,j,_mode);
+            float yPix = gradX->getPixel(i,j,_mode);
             float G = hypot(xPix,yPix);//sqrt(xPix*xPix + yPix*yPix);
             result->setPixel(i,j,G);
         }
     result->normalize();
     return result;
+}
+
+shared_ptr<Image> GaussFilterSep(const Image& _im, float _sigma, EdgeMode _mode)
+{
+    MaskFactory factory;
+    auto pair = factory.GaussSeparated(2);
+    return myIm->convolution(pair.first,pair.second,_mode);
+}
+
+shared_ptr<Image> GaussFilter(const Image& _im, float _sigma, EdgeMode _mode)
+{
+    MaskFactory factory;
+    return myIm->convolution(factory.Gauss(2),_mode);
+}
+
+void BuildPyramid(const Image& _im, float _sigma0, int _numLevels, EdgeMode _mode)
+{
+    //досглаживаем до sigma0
+    MaskFactory factory;
+    float deltaSigma = sqrt(_sigma0 * _sigma0 - 0,25);
+    shared_ptr<Image> curIm = GaussFilterSep(_im, deltaSigma, _mode);
+    //начинаем вычислять октавы
+    float k = pow(2,1./_numLevels);
+
+
 }
 
 int main()
