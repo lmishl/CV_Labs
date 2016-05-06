ea035da97912eccd01a7aa74d20235aaa14a3468
Laptev Mikhail <bugi-wugi93@mail.ru>
2016-05-06 10:42:02 +0700

    гавнамердж

--- main.cpp
+++ main.cpp
@@ -287,68 +287,64 @@ void DrawPanorama2(const Image &_im1, const Image &_im2, Transformation t, const
     result.save(_fileName);
 }
 
-Transformation Hough(const vector<pair<KeyPoint, KeyPoint>> &matches)
+void DrawModel(int x, int y, int a, float s, const Image &_im1, const Image &_im2, const QString &_fileName)
 {
-//параметры
-    float xSize = 2, ySize = 2, aSize = 10, sSize = 0.2;
+    QImage zaz = _im2.toQImage();
+    QPainter painter;
+    painter.begin(&zaz);
+    QPen qqq(QColor(rand() % 255, rand() % 255, rand() % 255));
+    painter.setPen(qqq);
 
-
-    //должно быть длина/xSize
-    int accum[300][300][360 / aSize][50];
-
-    for(uint i=0; i<matches.size(); i++)
-    {
-        KeyPoint p1 = matches[i].first;
-        KeyPoint p2 = matches[i].second;
-
-
-        float scale = p2.sigma / p1.sigma;
-
-        float angle = p2.angle - p1.angle;
-
-        //найдём левый верхний угол образца на 2ой картинке
-
-        //1 - повернули точку образца на угол angle
-        auto qq = rotate(0, 0, p1.globX(), p1.globY(), angle);
-
-        //2 - найдём начало образца на изобр-и
-        float x = p2.globX() - qq.first * scale;
-        float y = p2.globY() - qq.second  * scale;
-
-
-        //значения получили, раскидываем по корзинам
-        int xBin = floor(x / xSize + 0.5);
-        int yBin = floor(y / ySize + 0.5);
-        int aBin = floor(angle / aSize + 0.5);
-        int sBin = floor(scale / sSize + 0.5);
-
-         for(int xx = xBin - 1; xx <= xBin; xx++ )
-             for(int yy = yBin - 1; yy <= yBin; yy++ )
-                 for(int aa = aBin - 1; aa <= aBin; aa++ )
-                 {
-                     int ang = myProc(aa, aSize);
-                     for(int ss = sBin - 1; ss <= sBin; ss++ )
-                         accum[xx][yy][ang][ss]++;
-                 }
+    float scale = s;
+    float angle = a;
+    float x0 = x;
+    float y0 = y;
 
 
 
-    }
 
-    //ищем максимум
+    //теперь найдём другие 3 точки прямоугольника
+    int w = _im1.getWidth() * scale;
+    int h = _im1.getHeight() * scale;
 
-    //ищем инлаеров
+    // найдём повёрнутые координаты на образце
+    auto pp1 = rotate(0, 0, 0, w, -angle);
 
-    //строим модель
+    //найдём повёрнутые координаты на изобр-и
+    float x1 = x0 + pp1.first;
+    float y1 = y0 + pp1.second;
+
+    // найдём повёрнутые координаты на образце
+    auto pp2 = rotate(0, 0, h, w, -angle);
+
+    //найдём повёрнутые координаты на изобр-и
+    float x2 = x0 + pp2.first;
+    float y2 = y0 + pp2.second;
+
+    // найдём повёрнутые координаты на образце
+    auto pp3 = rotate(0, 0, h, 0, -angle);
+
+    //найдём повёрнутые координаты на изобр-и
+    float x3 = x0 + pp3.first;
+    float y3 = y0 + pp3.second;
 
 
-    return Transformation();
+    painter.drawLine(y0, x0, y1, x1);
+    painter.drawLine(y1, x1, y2, x2);
+    painter.drawLine(y2, x2, y3, x3);
+    painter.drawLine(y3, x3, y0, x0);
+
+    painter.end();
+    zaz.save(_fileName);
+
+
 }
 
-void DrawModels(shared_ptr<Image> myIm2, shared_ptr<Image> myIm1, vector<pair<KeyPoint, KeyPoint>> matches, const QString &_fileName)
+
+void DrawModels( const Image &myIm1,  const Image &myIm2, const vector<pair<KeyPoint, KeyPoint>> &matches, const QString &_fileName)
 {
 
-    QImage zaz = myIm2->toQImage();
+    QImage zaz = myIm2.toQImage();
     QPainter painter;
     painter.begin(&zaz);
 
@@ -375,12 +371,14 @@ void DrawModels(shared_ptr<Image> myIm2, shared_ptr<Image> myIm1, vector<pair<Ke
         float x0 = p2.globX() - pp0.first * scale;
         float y0 = p2.globY() - pp0.second * scale;
 
+         painter.drawEllipse(QPoint(y0, x0), 2, 2);
+
 
 
 
         //теперь найдём другие 3 точки прямоугольника
-        int w = myIm1->getWidth() * scale;
-        int h = myIm1->getHeight() * scale;
+        int w = myIm1.getWidth() * scale;
+        int h = myIm1.getHeight() * scale;
 
         // найдём повёрнутые координаты на образце
         auto pp1 = rotate(0, 0, 0, w, -angle);
@@ -416,6 +414,149 @@ void DrawModels(shared_ptr<Image> myIm2, shared_ptr<Image> myIm1, vector<pair<Ke
 
 }
 
+Transformation Hough(const vector<pair<KeyPoint, KeyPoint>> &_matches, const Image &_im1, const Image &_im2)
+{
+    //параметры
+    //x, y - по 2%    angle по 18градусов, scale по log2 + 2
+    int w = _im2.getWidth();
+    int h = _im2.getHeight();
+
+    //эта сука может падать, если размер будет больше
+    const int xBins = 50, yBins = 50, aBins = 20, sBins = 10;
+
+
+    //
+    int accum[xBins][yBins][aBins][sBins];
+    for(int a = 0; a < xBins; a++)
+        for(int b = 0; b < yBins; b++)
+            for(int c = 0; c < aBins; c++)
+                for(int d = 0; d < sBins; d++)
+                    accum[a][b][c][d] = -1;
+
+    vector<vector<uint>> votes;
+
+    int curList = 0;
+    for(uint i = 0; i < _matches.size(); i++)
+    {
+        KeyPoint p1 = _matches[i].first;
+        KeyPoint p2 = _matches[i].second;
+
+
+        float scale = p2.sigma / p1.sigma;//pow(2, p2.numberOctave - p1.numberOctave) * ();
+
+        float angle = p2.angle - p1.angle;
+
+        //найдём левый верхний угол образца на 2ой картинке
+
+        //1 - повернули точку образца на угол angle
+        auto pp0 = rotate(0, 0, p1.globX(), p1.globY(), -angle);
+
+        //2 - найдём начало образца на изобр-и
+        float x = p2.globX() - pp0.first * scale;
+        float y = p2.globY() - pp0.second * scale;
+
+
+        //log_a_(b) =  log_c_(b) / log_c_(a)
+
+        //значения получили, раскидываем по корзинам
+        int xBin = floor(x / h * xBins + 0.5);
+        int yBin = floor(y / w * yBins + 0.5);
+        int aBin = floor(angle / 360 * aBins + 0.5);
+        int sBin = floor(log2(scale) / log2(1.25) + 4.5); //log2(scale) + 2.5);
+
+        for(int xx = xBin - 1; xx <= xBin; xx++ )
+        {
+            if(xx >= xBins || xx < 0)
+                continue;
+            for(int yy = yBin - 1; yy <= yBin; yy++ )
+            {
+                if(yy >= yBins || yy < 0)
+                    continue;
+                for(int aa = aBin - 1; aa <= aBin; aa++ )
+                {
+                    int ang = myProc(aa, aBins);
+                    for(int ss = sBin - 1; ss <= sBin && ss < sBins && ss >= 0; ss++ )
+                    {
+                        if(ss >= sBins || ss < 0)
+                            continue;
+
+                        if(accum[xx][yy][ang][ss] == -1)
+                        {
+                            accum[xx][yy][ang][ss] = curList++;
+                            vector<uint> temp;
+                            temp.emplace_back(i);
+                            votes.emplace_back(temp);
+                        }
+                        else
+                            votes[accum[xx][yy][ang][ss]].emplace_back(i);
+                    }
+                }
+            }
+        }
+
+
+
+    }
+
+    //ищем максимум
+    int maxVec = 0;
+    int maxSize = votes[0].size();
+    for(uint i = 1; i < votes.size(); i++)
+    {
+        if(votes[i].size() >= maxSize)
+        {
+            maxSize = votes[i].size();
+            maxVec = i;
+            cout<<"max "<<maxSize<<endl;
+        }
+    }
+
+    //быдло вставка --- рисуем максимум
+    for(int a = 0; a < xBins; a++)
+        for(int b = 0; b < yBins; b++)
+            for(int c = 0; c < aBins; c++)
+                for(int d = 0; d < sBins; d++)
+                {
+                    if(maxVec == accum[a][b][c][d])
+                    {
+                        cout<<h<<"  "<<w<<endl;
+
+
+                        int xx = 1.0 * h / xBins * (a + 0.5);
+                        int yy = 1.0 * w / yBins * (b + 0.5);
+                        int aa = 360.0 / aBins * (c + 0.5);
+                        float ss =  pow(1.25, d - 3.5);// * sqrt(2);
+                        cout<<xx<<"  "<<yy<<"  "<<aa<<"  "<<ss<<endl<<"---------"<<endl;
+                        DrawModel(xx, yy, aa, ss, _im1, _im2, "C:\\9\\bydlo.png");
+                        cout<<a<<"  "<<b<<"  "<<c<<"  "<<d<<endl;
+
+                        break;
+                    }
+                }
+
+
+
+    //Соберём вектор соответствий
+    vector<pair<KeyPoint, KeyPoint>> vec;
+    for(uint i = 1; i < votes[maxVec].size(); i++)
+        vec.emplace_back(_matches[votes[maxVec][i]]);
+
+
+    //выводим что нашли
+    DrawMatches(_im1, _im2, vec, "C:\\9\\votes.png");
+    DrawModels(_im1, _im2, vec, "C:\\9\\votesModels.png");
+
+
+
+
+    return Transformation(vec);
+}
+
+
+
+
+
+
 int main()
 {
     unsigned int start_time =  clock(); // начальное время
@@ -427,10 +568,10 @@ int main()
 
 
 
-    vector<Descriptor> descs1 = findBlobs(*myIm1->ot0do1(), 1, "C:\\9\\blob1.tif");
+    vector<Descriptor> descs1 = findBlobs(*(myIm1->ot0do1()), 0, "C:\\9\\blob1.tif");
     cout<<"blob1  "<< (int)clock() - start_time<<endl;
 
-    vector<Descriptor> descs2 = findBlobs(*myIm2->ot0do1(), 1, "C:\\9\\blob2.tif");
+    vector<Descriptor> descs2 = findBlobs(*(myIm2->ot0do1()), 0, "C:\\9\\blob2.tif");
     cout<<"blob2  "<< (int)clock() - start_time<<endl;
 
 
@@ -440,9 +581,11 @@ int main()
     vector<pair<KeyPoint, KeyPoint>> matches = FindMatches(descs1, descs2);
     DrawMatches(*myIm1, *myIm2, matches, "C:\\9\\Un.png");
 
-    DrawModels(myIm2, myIm1, matches, "C:\\9\\temp.png");
+    DrawModels(*myIm1, *myIm2, matches, "C:\\9\\temp.png");
+    Transformation t = Hough(matches, *myIm1, *myIm2);
+   // Transformation t = Ransac(matches);
 
-    // DrawPanoramaColor(fileName1, fileName2, t, "C:\\9\\Pan2.png");
+    DrawPanoramaColor(fileName1, fileName2, t, "C:\\9\\Pan2.png");
 
     unsigned int search_time = (int)clock() - start_time; // искомое время
     cout<<"\ngood "<< search_time<<endl;
